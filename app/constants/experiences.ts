34109e8afeb902666033d0a38eb5101cc55ab442
Om Sarraf <itsomsarraf+github@gmail.com>
2024-04-13 17:25:51 +0530

    experiences

---
+++ app/constants/experiences.ts
@@ -0,0 +1,34 @@
+export const experiences = {
+	Intern0: {
+		designation: 'Founding Engineer @ FOF.link',
+		day: '1',
+		month: ['Apr', 'May'],
+		year: [2024, 'Present'],
+		location: 'Remote',
+		more: ''
+	},
+	College: {
+		designation: 'GDSC Lead',
+		day: '1',
+		month: ['July', 'July'],
+		year: [2023, '2024'],
+		location: 'College -Greater Noida',
+		more: ''
+	},
+	Community: {
+		designation: 'Core Team GDG Noida/New Delhi',
+		day: '1',
+		month: ['Dec', 'Pesent'],
+		year: [2022, 'Present'],
+		location: 'Delhi + Noida',
+		more: ''
+	},
+	School: {
+		designation: 'IT Council President',
+		day: '1',
+		month: ['July', 'July'],
+		year: [2020, 2021],
+		location: 'School (Delhi)',
+		more: ''
+	}
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,40 @@
+import { experiences } from "./constants/experiences";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio ( building 🏗️🛠️)
+        My Portfolio (building 🏗️🛠️)
       </h1>
       <p className="mb-4">
         {`Om Sarraf is an individual from India who is engaged in various activities such as freelancing, learning Next + Ts, watching anime, and reading books. He is also associated with different organizations like GDSC, NASA, and holds positions such as GDSC Lead 2023-24 and President of the IT council of DPS Dwarka.`}
       </p>
-      <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-        <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-          April 1, 2024
-        </p>
-        <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-          ????
-        </p>
+      <div className="w-full flex flex-col space-x-0">
+        {Object.entries(experiences).map(([experience, { designation, month, year }]) => {
+          const yearStr0 = String(year[0])
+          year[0] = yearStr0.slice(-2)
+          const isOngoing = year[1] === 'Present' || month[1] === 'Present';
+          let ongoingText = isOngoing ? 'Present' : `${month[1]} '${year[1]}`;
+          if (ongoingText !== 'Present') {
+            const yearStr1 = String(year[1])
+            year[1] = yearStr1.slice(-2)
+          }
+          return (
+            <div key={experience} className="flex flex-row justify-between w-full">
+              <div className="flex flex-row items-center">
+                <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
+                  {month[0]} '{year[0]} - {ongoingText}
+                </p>
+              </div>
+              <div className="flex flex-row items-center">
+                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                  {designation}
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </div>
-
     </section>
-  )
+  );
 }
