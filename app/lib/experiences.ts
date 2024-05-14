28d0b7f08bc30c040a5ce5d416bba58628c02d04
Om Sarraf <itsomsarraf+github@gmail.com>
2024-05-15 02:51:45 +0530

    card update

---
+++ app/lib/experiences.ts
@@ -0,0 +1,57 @@
+export const experiences = [
+	{
+		title: 'FullStack Intern',
+		company: 'vexio.in',
+		location: 'Remote',
+		duration: 'May 2024 - Present',
+		Sdate: 'May 1, 2024',
+		Edate: 'Present',
+		description:
+			'Working with a small team of 6 people on the GenAi MVP, stack: Nextjs and Django',
+		url: 'https://www.vexio.in'
+	},
+	{
+		title: 'Solo Founding Dev',
+		company: 'FOF.link',
+		location: 'Remote',
+		duration: 'April 2024 - May 2024',
+		Sdate: 'April, 2024',
+		Edate: 'May 2024',
+		description:
+			'Working as a solo Engineer at FOF.link, learnt Nextjs on day 0 of joining, building the platform solely :D',
+		url: 'https://www.fof.link'
+	},
+	{
+		title: 'GDSC Lead',
+		company: 'Bennett University',
+		location: 'Bennett University',
+		duration: 'July 2023 - July 2024',
+		Sdate: 'July, 2023',
+		Edate: 'July 2024',
+		description:
+			'Led the Google Developer Student Club at college, organizing coding events and workshops.',
+		url: 'https://www.bennett.edu.in'
+	},
+	{
+		title: 'Google Developer Groups',
+		company: 'Google Developer Group',
+		location: 'Delhi NCR',
+		duration: 'December 2022 - Present',
+		Sdate: 'December, 2022',
+		Edate: 'Present',
+		description:
+			"Part of the core team for the Google Developer Group in Noida and New Delhi, helping organize DevFest '22, '23",
+		url: 'https://developers.google.com/community/gdg'
+	},
+	{
+		title: 'President, IT Council',
+		company: 'DPS Dwarka',
+		location: 'DPS Dwarka (School)',
+		duration: 'July 2020 - July 2021',
+		Sdate: 'July, 2020',
+		Edate: 'July 2021',
+		description:
+			'Served as the President of the IT Council in school, leading technology initiatives and events.',
+		url: 'https://www.dpsdwarka.com'
+	}
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 "use client"
 
-import { experiences } from "./constants/experiences";
-import React, { useState, useRef, useEffect } from "react";
+import Component from "./components/detailscard";
+import { experiences } from "./lib/experiences";
+import React, { useState, useRef, useEffect, RefObject } from "react";
 
+// Define the type for Experience
+type Experience = {
+  title: string;
+  company: string;
+  location: string;
+  duration: string;
+  Sdate: string;
+  Edate: string;
+  description: string;
+  url: string;
+};
 
 export default function Page() {
+  const [selectedExperience, setSelectedExperience] = useState<Experience | null>(null);
 
-  const [selectedExperience, setSelectedExperience] = useState<string | null>(null);
   return (
     <section>
       <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
@@ -16,29 +28,34 @@ export default function Page() {
         {`Om Sarraf is an individual from India who is engaged in various activities such as freelancing, learning new technologies, watching anime, and reading books.`}
       </p>
       <p className="text-xl">Experience:</p>
-      <div className="w-full md:flex-col flex-row space-x-0">
-        {Object.entries(experiences).map(([experience, { designation, month, year }]) => {
-          const isOngoing = year[1] === 'Present' || month[1] === 'Present';
-          let ongoingText = isOngoing ? 'Present' : `${month[1]} '${year[1]}`;
-
-          return (
-            <div key={experience} className="flex justify-between w-full mt-5" onClick={() => setSelectedExperience(experience)}>
-              <div className="flex flex-row items-center text-nowrap text-left">
-                <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
-                  {month[0]} '{year[0]} - {ongoingText}
-                </p>
-              </div>
-              <div className="flex flex-row items-center">
-                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight text-balance md:text-nowrap text-right underline cursor-pointer">
-                  {designation}
-                </p>
-              </div>
+      <div className="w-full flex flex-col space-y-5">
+        {experiences.map((experience, index) => (
+          <div
+            key={index}
+            className="flex justify-between w-full mt-5 cursor-pointer"
+            onClick={() => setSelectedExperience(experience)}
+          >
+            <div className="flex flex-row items-center text-nowrap text-left">
+              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
+                {experience.duration}
+              </p>
             </div>
-          );
-        })}
+            <div className="flex flex-row items-center">
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight text-balance md:text-nowrap text-right underline">
+                {experience.title}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
-      {selectedExperience && (<Overlay isVisible={true}><ExperienceDetailsPopup experience={experiences[selectedExperience]} setSelectedExperience={setSelectedExperience} />
-      </Overlay>)}
+      {selectedExperience && (
+        <Overlay isVisible={true}>
+          <ExperienceDetailsPopup
+            experience={selectedExperience}
+            setSelectedExperience={setSelectedExperience}
+          />
+        </Overlay>
+      )}
     </section>
   );
 }
@@ -46,15 +63,12 @@ export default function Page() {
 function Overlay({ isVisible, children }) {
   if (!isVisible) return null;
 
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 z-40 w-full">
-      {children}
-    </div>
-  );
+  return <div className="fixed inset-0 bg-black bg-opacity-50 z-40 w-full">{children}</div>;
 }
 
-function ExperienceDetailsPopup({ experience, setSelectedExperience }: { experience: any, setSelectedExperience: (experience: string | null) => void }) {
-  const popupRef = useRef<HTMLDivElement | null>(null);
+function ExperienceDetailsPopup({ experience, setSelectedExperience }) {
+  const popupRef: RefObject<HTMLDivElement> = useRef(null);
+
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
@@ -73,26 +87,23 @@ function ExperienceDetailsPopup({ experience, setSelectedExperience }: { experie
       ref={popupRef}
       className="fixed inset-0 flex items-center justify-center z-50 p-4 backdrop-blur-sm bg-black/30"
     >
-      <div className="bg-white rounded-lg shadow-lg mx-auto relative p-6 max-w-96">
-        <h2 className="text-2xl font-bold mb-2 text-black">{experience.designation}</h2>
-        <div className="flex gap-10 mb-2 text-wrap">
-          <p className="text-sm text-neutral-600 mb-1">
-            <span className="font-semibold"></span>📍: {experience.location}
-          </p>
-          <p className="text-sm text-neutral-600 mb-1">
-            <span className="font-semibold">Timeline : </span>{experience.month[0]}'{experience.year[0]} - {experience.year[1] === 'Present' ? 'Present' : `20${experience.year[1]}`}
-          </p>
-        </div>
-        <p className="text-md text-neutral-800 text-wrap font-sans">{experience.more}</p>
-      </div>
+      <Component
+        duration={experience.duration}
+        title={experience.title}
+        location={experience.location}
+        Sdate={experience.Sdate}
+        Edate={experience.Edate}
+        company={experience.company}
+        description={experience.description}
+        url={experience.url}
+      />
       <button
         onClick={() => setSelectedExperience(null)}
-        className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-neutral-300 cursor-pointer px-3 py-1 rounded mb-40 hover:underline "
+        className="absolute top-4 right-4 text-neutral-300 cursor-pointer px-3 py-3 rounded hover:underline"
         style={{ zIndex: 1000 }}
       >
-        Close
+        X
       </button>
     </div>
   );
 }
-
