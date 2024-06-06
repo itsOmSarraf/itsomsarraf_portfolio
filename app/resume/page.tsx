f52bebbfa3523adb87f241b562dec2eb071a4f5f
Om Sarraf <itsomsarraf+github@gmail.com>
2024-06-06 13:44:55 +0530

    Update page.tsx

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,7 @@
 export default function Resume() {
     return (
         <div>
-            <a href='https://docs.google.com/document/d/1TBwnzMJK5JbVrCTMpFI89YLtPMC3aloTEbMxlDcHB00/edit?usp=sharing' target="_blank" rel="noopener noreferrer" className="underline">
+            <a href='https://drive.google.com/file/d/19HNHGVynBwxO7MQdQGiKbPfOYcnow21n/view?usp=sharing' target="_blank" rel="noopener noreferrer" className="underline">
                 Gdrive &#8599;
             </a>
         </div>
