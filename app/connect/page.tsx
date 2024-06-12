9b45f4a720dea5697144643e4edb0309a8301de3
Om Sarraf <itsomsarraf+github@gmail.com>
2024-06-12 10:25:05 +0530

    Update page.tsx

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -1,8 +1,7 @@
 export default function Connect() {
     return (
         <div>
-            Google my
-            <a href="https://www.google.com/search?q=Om+Sarraf" target="_blank"> <span className="underline">name</span></a>
+            <a href="https://www.google.com/search?q=Om+Sarraf" target="_blank"> <span className="underline">Here</span></a>
         </div>
     )
 }
