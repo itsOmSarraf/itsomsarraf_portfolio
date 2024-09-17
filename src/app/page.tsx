d5b8646d50dec7184a3dea07ac9f9ae60a8b2f69
Om Sarraf <itsomsarraf+github@gmail.com>
2024-09-17 14:02:28 +0530

    build fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Projects from "@/components/Projects";
 import Hero from "../components/Hero";
-import Socials from "./Socials";
 import Experiences from "@/components/Experience";
 
 export default function Home() {
