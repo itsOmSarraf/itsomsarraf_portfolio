5041af27c8259f331e322d2a96feb305700e0251
Om Sarraf <itsomsarraf+github@gmail.com>
2024-09-17 19:15:49 +0530

    build fixes v0.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Projects from "@/components/Projects";
+// import Projects from "@/components/Projects";
 import Hero from "../components/Hero";
 import Experiences from "@/components/Experience";
 
