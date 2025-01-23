049f0244414ada823d9c1709fed0edd98b39a5cf
Om Sarraf <itsomsarraf+github@gmail.com>
2025-01-23 15:13:30 +0530

    resume

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Resume() {
+    return (
+        <div className="w-full h-screen flex flex-col items-center gap-4">
+            <Link
+                href="https://drive.google.com/uc?export=download&id=1g4y6HcB1YGeSlEwVrUvx_3rQ43boEY7r"
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+            >
+                Download Resume
+            </Link>
+            <iframe
+                src="https://drive.google.com/file/d/1g4y6HcB1YGeSlEwVrUvx_3rQ43boEY7r/preview"
+                width="70%"
+                height="90%"
+                allow="autoplay"
+                className="border-none"
+            ></iframe>
+        </div>
+    );
+}
