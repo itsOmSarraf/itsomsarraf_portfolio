b1fe1950d07dea070f4fc33c3295462840ee8c57
Om Sarraf <itsomsarraf+github@gmail.com>
2025-04-08 22:34:02 +0530

    Update page.tsx

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -6,7 +6,7 @@ export default function Resume() {
     return (
         <div className="w-full h-screen flex flex-col items-center gap-4">
             <Link
-                href="https://drive.google.com/uc?export=download&id=1cv7tKHqGCihcCDnWnzmJArGXAjRN-Nan/"
+                href="https://drive.google.com/uc?export=download&id=1cv7tKHqGCihcCDnWnzmJArGXAjRN-Nan"
                 className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
             >
                 Download Resume
