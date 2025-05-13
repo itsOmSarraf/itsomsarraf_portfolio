0853043ae957d54f02aec6bc6cbbe81013855cef
Om Sarraf <itsomsarraf@gmail.com>
2025-05-13 18:12:26 +0530

    resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -6,18 +6,19 @@ export default function Resume() {
     return (
         <div className="w-full h-screen flex flex-col items-center gap-4">
             <Link
-                href="https://drive.google.com/uc?export=download&id=1cv7tKHqGCihcCDnWnzmJArGXAjRN-Nan"
+                href="https://drive.google.com/uc?export=download&id=1bmrHuYXWg1XsBgdFQjAMvHhjphsC0kKp"
                 className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
             >
                 Download Resume
             </Link>
             <iframe
-                src="https://drive.google.com/file/d/1cv7tKHqGCihcCDnWnzmJArGXAjRN-Nan/preview"
+                src="https://drive.google.com/file/d/1bmrHuYXWg1XsBgdFQjAMvHhjphsC0kKp/preview"
                 width="70%"
                 height="90%"
                 allow="autoplay"
                 className="border-none"
-            ></iframe>
+                title="Resume"
+            />
         </div>
     );
 }
